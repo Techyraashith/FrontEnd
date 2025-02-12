5d7cd26d3a63a43eb17c20d42e5116c95656975f
Techyraashith <raashithmohammed305@gmail.com>
2025-02-12 21:51:13 +0530

    CSS Prject

---
+++ Javascript'/day1.js
@@ -0,0 +1,24 @@
+let a = 10;
+let b = 20;
+
+console.log(a+b);
+
+let object = {name : "Raashith", Age : 19, gender:"Male"}
+
+console.log(object);
+
+
+let object1 = {
+        name: "Raashith", 
+        dep1: "Data Analyst", dep2: {
+            depName: "Fullstack"
+        }
+    };
+
+console.log(object1);
+console.log(object1.dep1);
+console.log(object1.dep2.depName);
+
+
+
+
